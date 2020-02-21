6fd1e540db21c223c531a362435cae026a2be7db
shibatatsuya0611 <55233878+shibatatsuya0611@users.noreply.github.com>
2020-02-21 11:46:25 +0700

    Initial project setup

---
+++ LibSwift/LibSwift.h
@@ -0,0 +1,19 @@
+//
+//  LibSwift.h
+//  LibSwift
+//
+//  Created by Xuan Huy on 2/21/20.
+//  Copyright © 2020 Xuan Huy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibSwift.
+FOUNDATION_EXPORT double LibSwiftVersionNumber;
+
+//! Project version string for LibSwift.
+FOUNDATION_EXPORT const unsigned char LibSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibSwift/PublicHeader.h>
+
+
