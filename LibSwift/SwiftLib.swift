b7d8799e325cecd6ab1e3a8671dcc74a34e4cdcc
shibatatsuya0611 <55233878+shibatatsuya0611@users.noreply.github.com>
2020-02-21 14:43:22 +0700

    Added SwiftLib.podspec

---
+++ LibSwift/SwiftLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftLib.swift
+//  LibSwift
+//
+//  Created by Xuan Huy on 2/21/20.
+//  Copyright © 2020 Xuan Huy. All rights reserved.
+//
+
+import Foundation
+public final class SwiftLib
+{
+
+    let name = "SwiftLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- LibSwiftTests/LibSwiftTests.swift
+++ LibSwiftTests/LibSwiftTests.swift
@@ -9,26 +9,20 @@
 import XCTest
 @testable import LibSwift
 
-class LibSwiftTests: XCTestCase {
+class LibSwiftTests: XCTestCase
+{
 
+    var swiftLib: SwiftLib!
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftLib = SwiftLib()
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd()
+    {
+        XCTAssertEqual(swiftLib.add(a: 1, b: 1), 2)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testSub()
+    {
+        XCTAssertEqual(swiftLib.sub(a: 2, b: 1), 1)
     }
 
 }
